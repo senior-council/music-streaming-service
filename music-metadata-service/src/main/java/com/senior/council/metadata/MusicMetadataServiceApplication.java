96faed24936318f50806b42f28babc3814611932
zhenyashvyrkov <zhenya100694@mail.ru>
2023-08-01 10:23:32 +0300

    init

---
+++ music-metadata-service/src/main/java/com/senior/council/metadata/MusicMetadataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.senior.council.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMetadataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicMetadataServiceApplication.class, args);
+    }
+
+}
---
+++ search-service/src/main/kotlin/com/senior/council/search/SearchServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.senior.council.search
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SearchServiceApplication>(*args)
+}
